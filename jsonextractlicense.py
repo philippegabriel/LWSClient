4a0321d416c47d7fe47f76deba14a604cd52458e
philippe gabriel <philippe.gabriel@citrix.com>
2015-12-14 14:33:42 +0000

    Make error sent back from LWS more obvious

--- jsonextractlicense.py
+++ jsonextractlicense.py
@@ -11,6 +11,12 @@ fd=open(input)
 input=fd.read()
 decoder=json.JSONDecoder(strict=False)
 dict=decoder.decode(input)
+if('errorMessage' in dict):
+	print("############################### LWS returned an ERROR ######################################")
+	for k, v in dict.iteritems():
+    		print(k, v)
+	print("############################################################################################")
+	sys.exit(1)
 print("RequestUUID:%s"%dict['RequestUUID'])
 print("Created:%s"%dict['Created'])
 print("from IP address:%s"%dict['Source IP'])
